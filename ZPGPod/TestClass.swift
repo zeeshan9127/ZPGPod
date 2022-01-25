4944ee3908d0dcff41981c84282da8baa385d8a9
Muhammad Zeeshan <zeeshanmuhammad@etisalat.ae>
2022-01-25 15:51:26 +0500

    - add pod classes

---
+++ ZPGPod/TestClass.swift
@@ -0,0 +1,22 @@
+//
+//  testClass.swift
+//  TestSDK
+//
+//  Created by Muhammad Zeeshan on 11/01/2022.
+//
+
+import Foundation
+
+public final class TestClass {
+
+    let name = "testClass"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- ZPGPodTests/ZPGPodTests.swift
+++ ZPGPodTests/ZPGPodTests.swift
@@ -10,24 +10,17 @@ import XCTest
 
 class ZPGPodTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var testClass: TestClass!
+    
+    override func setUp() {
+        testClass = TestClass()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(testClass.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(testClass.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
