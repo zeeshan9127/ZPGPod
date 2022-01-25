4c6f0a7c53f5c64c4f37a71dddcec087ad648654
Muhammad Zeeshan <zeeshanmuhammad@etisalat.ae>
2022-01-25 15:39:21 +0500

    Initial Pod setup

---
+++ ZPGPod/ZPGPod.h
@@ -0,0 +1,18 @@
+//
+//  ZPGPod.h
+//  ZPGPod
+//
+//  Created by Muhammad Zeeshan on 25/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZPGPod.
+FOUNDATION_EXPORT double ZPGPodVersionNumber;
+
+//! Project version string for ZPGPod.
+FOUNDATION_EXPORT const unsigned char ZPGPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZPGPod/PublicHeader.h>
+
+
